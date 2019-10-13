759e6542d517338222715823d3abd9a7beb7e89a
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2019-10-13 23:02:24 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,54 +24,78 @@ client.on('message', message => { // When the message a message is executed
 		}, delayInMilliseconds);	   
 
        }*/
-    if (!(message.author.bot) && (message.content === 'f' || message.content === 'F')) { // if that message matches ping
+	if (!message.author.bot) {
+    if ((message.content === 'f' || message.content === 'F')) { // if that message matches ping
 
        message.reply('https://i0.wp.com/jojo-fan.com/wp-content/uploads/2015/05/0e8813c4280abc24d7180a8f1c92f50d.jpg');
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);	
 
        }   
-    if (!(message.author.bot) && message.content.includes('69')) { // if that message matches ping
+    if (message.content.includes('69')) { // if that message matches ping
 		message.reply('HAHAHAHA fucking hilarious! Get it? It\'s the sex number, therefore it\'s funny! Except it\'s not fucking funny, how much of a braindead person do you have to be to find the number sixty-nine funny? How old are you people? Get a fucking grip on reality you mongoloid bastards. This is why i fucking despise meme culture sometimes, it promotes braindead humor for 3 year olds. "Nice nice nice" shut the fuck up you prick, repeating a word is not funny either. Holy fuck i\'d actually punch someone in the face IRL for acting like such a retard.');
-       }
-    if (!(message.author.bot) && (message.content.toLowerCase() === ('oof') || message.content.toLowerCase() === ('oof 100'))) { // if that message matches ping
+       	var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
+	   }
+    if ((message.content.toLowerCase() === ('oof') || message.content.toLowerCase() === ('oof 100'))) { // if that message matches ping
 
        message.reply('Shut the fuck up you absolute lobotomite. You think adding your poorly edited Skyrim makes this funny? What a joke. Get a grip on reality, stop saying "oof" during a serious conversation just because your two braincells cant ever send signals to eachother to form a response. Honestly pathetic. Oh guys look at me Im so funny OOF 100! Its so funny and quirky because its a roblox noise right guys? Hahah skyrim reference!! Shut the fuck up you absolute fucking dusty toilet. I would flush you down the drain in an instant.');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-    if (!(message.author.bot) && message.content.toLowerCase().includes('fortnite bad')) { // if that message matches ping
+    if (message.content.toLowerCase().includes('fortnite bad')) { // if that message matches ping
 
        message.reply('DUHHHHH FORTNITE BAD DUHHHH BRORTNITE BRAD!!1!1 IS THAT ALL YOU DEPRESSED FUCKS CAN SAY? FORTNITE FORTNITE EMOJI INSTA BAD EMOJI EMOJI BAD BAD!!1!1 I FEEL LIKE IM IN A FUCKING ASYLUM FULL OF DEMENTIA RIDDEN OLD PEOPLE WHO CAN DO NOTHING BUT REPEAT THE SAME FUCKING WORDS ON LOOP LIKE A BROKEN FUCKING RECORD');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-    if (!(message.author.bot) && (message.content.toLowerCase().includes('war') || message.content.toLowerCase().includes('violence'))) { // if that message matches ping
+    if ((message.content.toLowerCase().includes('war') || message.content.toLowerCase().includes('violence'))) { // if that message matches ping
 
        message.reply('Did you know, the first ever Call of Duty game released in 2003, a whopping 4713 years after the first ever recorded war, "Sumer vs. Elam", which occurred in 2700 BC. This is strange, considering that violence didn\'t exist before Call of Duty. It is theorized that violent video games existed in that period, as it would be impossible for any type of violence to happen if they were not. In late 2012, a paleontologist who was digging into a random patch of land, found a strange rock case, when it was cracked open, a disc titled "Call of Duty: Before Christ" was inside, but the disc wasn\'t able to be read, despite being tested in various disc readers. We can however speculate, that this is what led to violence in our world, starting with the "Sumer vs. Elam" war, and continuing, with war looming over us to this day.');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-   if (!(message.author.bot) && message.content.toLowerCase() === ('k')) { // if that message matches ping
+   if (message.content.toLowerCase() === ('k')) { // if that message matches ping
 
        message.reply('K? Really you fucking idiot? I pour my heart and soul into a message and you reply with k? Do you think this is some kind of joke? Do you not realize how much this hurt me? This is like giving a man you risked his life in combat to save 100 other soldiers and giving him a bar of chocolate. God you are so inconsiderate. Come back when you learn to text like a normal person.');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-   if (!(message.author.bot) && (message.content.toLowerCase().includes('torture') || message.content.toLowerCase().includes('cock and ball') || message.content.toLowerCase().includes('cbt'))) { // if that message matches ping
+   if ((message.content.toLowerCase().includes('torture') || message.content.toLowerCase().includes('cock and ball') || message.content.toLowerCase().includes('cbt'))) { // if that message matches ping
 
        message.reply('Why do people enjoy cock and ball torture? The act of intentionally or accidentally bringing pain upon the male genitals is typically a thought that people wince at with great force. However, there is a reason why this is an enjoyable experience. One rooted in the most important of sciences. Physics. Take, for example, a bottle of Tobasco Sauce. If one where to move the bottle up and down in a motion reminiscent to the one used during male self-stimulation it typically results in a moderate amount of sauce exiting the bottle. If struck hard on the bottom, however, a large amount will spurt out. Should this be done with the name genitals, turning them upside down and then striking the bottom of the scrotum with moderate to immense force, this will result in a large amount of seed to be ejected from the penis due to the energy being transferred from the palm of the hand to genitals. Therefore, because of physics and the transfer of energy from palm to the genitals, cock and ball torture is a scientifically pleasurable experience.');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-    if (!(message.author.bot) && message.content.toLowerCase().includes('shuckle')) { // if that message matches ping
+    if (message.content.toLowerCase().includes('shuckle')) { // if that message matches ping
 
        message.reply('Shuckle is the worst pokemon.It\'s horrendously ugly and utterly useless. I hate it. What fucking niche does Shuckle have in BH? It just sits there and does nothing. I hate it. It provokes a deep rooted anger within me whenever I see it. I want to drive on over to the fucking pokemon headquarters and kill it. If this was the pokemon world, I\'d push it off a fucking cliff. Noobs just bring shuckle as if it\'s actually useful. It\'s not. Shuckle deserves to die. He deserves to have his smug little yellow face smashed in with a hammer. Oh wow, it\'s a fucking turtle with atrocious hp and horrendous typing, how fucking useful, I\'ll use it in every BH team I make. NO. STOP IT. It deserves to burn in hell. Why is it so goddamn smug. You\'re a fucking mold, you have no life goals, you will never accomplish anything in life apart from pissing me off. When you die noone will mourn. I hope you die');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
 	}
-    if (!(message.author.bot) && (message.content.toLowerCase().includes('deod') || message.content.toLowerCase().includes('deoxys-defense') || message.content.toLowerCase().includes('deoxys defense'))) { // if that message matches ping
+    if ((message.content.toLowerCase().includes('deod') || message.content.toLowerCase().includes('deoxys-defense') || message.content.toLowerCase().includes('deoxys defense'))) { // if that message matches ping
 
        message.reply('Okay so I was just peacefully walking down the road as I usually do, minding my own business. Then all of a sudden a little disgusting 8-year old disabled kid suddenly logged in to PS on his phone, and started using DeoD in BH! I was furious! But I calmed down and got a hold of myself. Even though using DeoD is a horrible crime, he was only a child and mentally ill at that. But then he found a replay of Simple Smash DeoD sweeping, and he showed his mum! My entire body was shaking!!! How dare he offend me like that and spread pure evil into the world? So I walked over to him, and punched him so hard he was doing the wormdance on the ground. And then I shouted "DEOD BAD CRESSELIA GOOD!!!!" and then I trampled him to death while shitting profusely all over his corpse. Now obviously I expected that everyone would applause me for hindering the spread of evil and for doing gods work, but for some weird reason everyone just started screaming, and after a couple of minutes the police arrived and they arrested me?!?! What is happening??? The trial will begin soon, so I just wanna know, Am I The Asshole?');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
-    if (!(message.author.bot) && (message.content.toLowerCase().includes('uturn') || message.content.toLowerCase().includes('u-turn'))) { // if that message matches ping
+    if ((message.content.toLowerCase().includes('uturn') || message.content.toLowerCase().includes('u-turn'))) { // if that message matches ping
 
        message.reply('Day 1035 of Gen 7 BH. All of my teams lose to U-turn. Whenever I load up a team on the BH ladder, I\'m terrified that I\'ll run into a U-turn team. Fearing the worst, I have taken every possible countermeasure. My teams are now built exclusively with 4x bug resists. But still, U-turn is too powerful for me. As soon as I see the Registeel hit my Ho-Oh for 3%, I know the game is over right there and just click X before all my Pokemon inevitably die. Skylake is telling me to adapt but I don\'t know if I can anymore. I tried running Flame Body on my Togekiss but it\'s only a 30% chance that I\'m even able to punish the move. I urge every one of you to get this move out of the metagame immediately, because I don\'t think I can take it any more.');	   
-
+	   var delayInMilliseconds = 600000; //10 minutes
+		setTimeout(function() {
+		}, delayInMilliseconds);
        }
+	}
        });
 
  
