f8cb802de34816e58dfbf5257adcf70ca570ac43
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2019-12-08 20:27:39 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,72 +13,72 @@ client.on('ready', () => { // know if our bot is online
 
 client.on('message', message => { // When the message a message is executed
 	
-	var postedRecently = false;
 	
     /*if (message.content === 'f' || message.content === 'F') { // if that message matches ping
 
        message.reply('F? F what? The letter before G? The letter after E? Did you know that in Fu the F  stands for “fuck?” So your reply is “fuck?” or F as in Flourine? Do you need some Special F for breakfast? F as in I can fuck you? Can I fuck you and feed you to hungry falcons? Falcons have an f in it. "F”? Are you fucking kidding me? I spent a decent portion of my life writing all of that and your response to me is "F”?Are you so mentally handicapped that the only letter you can comprehend is "F” - or are you just some fucking asshole who thinks that with such a short response, he can make a statement about how meaningless what was written was? Well, I\'ll have you know that what I wrote was NOT meaningless, in fact, I even had my written work proof-read by several professors of literature. Don\'t believe me? I doubt you would, and your response to this will probably be "F” once again. Do I give a fuck? No, does it look like I give even the slightest fuck about a single letter? I bet you took the time to type that one letter too, I bet you sat there and chuckled to yourself for 20 hearty seconds before pressing "send". You\'re so fucking pathetic. I\'m honestly considering directing you to a psychiatrist, but I\'m simply far too nice to do something like that. You, however, will go out of your way to make a fool out of someone by responding to a well-thought-out, intelligent, or humorous statement that probably took longer to write than you can last in bed with a chimpanzee. What do I have to say to you? Absolutely nothing. I couldn\'t be bothered to respond to such a worthless attempt at a response. Do you want "F” on your gravestone? Do you want people to remember you as the asshat who one day decided to respond to someone with a single letter? "Hey, look, everybody! It\'s that "f” guy!" That\'s who you are. You\'re going to be known as the "F” guy. How does it feel? Do you feel happy? Quite honestly, I don\'t care, which is why I\'m not even going to respond to you.');
 		
-		var delayInMilliseconds = 100; //0.1 second
-		setTimeout(function() {
-			message.reply('Goodbye, and good luck with your future as that guy who said "F”. Alright listen up motherfucker, and get your comfy seat because we\'re gonna be here a while. Do you really think you can just get away with "f" as a message? What if someone did that to you, huh? Do you think you would like it? Making an entire paragraph to get a fuckin\' one letter response of the sixth letter in the alphabet, you think that\'s fuckin\' funny, jackass? Do you want your crush to respond back with "f” after you spill your feelings out like this? (Take me back, Emma.) Huh? What if I did it to you? F. Did you fuckin\' like that? What, did you just jizz in your pants because someone disregarded your entire effort of writing this ENTIRE paragraph FROM HAND in about fifteen minutes? That just makes me feel fucking rejected just like my ex. (Take me back, Roxanne.) f. What\'re you, fuckin\' gay? Can I have a response that actually MEANS something instead of just shitfacing our "conversation" with the spam of "f?”. Now occasionally with questions or something it\'s reasonable, but doing it to any fucking response they say. "We\'re having a nuclear crisis, you have fifteen minutes to evacuate." You\'re the type of person that would fuckin\' say "f” to that, you limp dick hypocrite. You think you can get away with this, right? You think it\'s SOOOOOO funny to do this shit, but I can guarantee that you\'ll be taken out back and shot soon. You\'re fucking dead, "f”ucker.');
-		}, delayInMilliseconds);	   
-
        }*/
-	if (!message.author.bot && !postedRecently) {
+	if (!message.author.bot) {
+		
     if ((message.content === 'f' || message.content === 'F')) { // if that message matches ping
-
        message.reply('https://i0.wp.com/jojo-fan.com/wp-content/uploads/2015/05/0e8813c4280abc24d7180a8f1c92f50d.jpg');
-       postedRecently = true;
+	     setTimeout(() => {
+        }, 43200000); //12hours
        }   
     if (message.content.includes('69')) { // if that message matches ping
-		message.reply('HAHAHAHA fucking hilarious! Get it? It\'s the sex number, therefore it\'s funny! Except it\'s not fucking funny, how much of a braindead person do you have to be to find the number sixty-nine funny? How old are you people? Get a fucking grip on reality you mongoloid bastards. This is why i fucking despise meme culture sometimes, it promotes braindead humor for 3 year olds. "Nice nice nice" shut the fuck up you prick, repeating a word is not funny either. Holy fuck i\'d actually punch someone in the face IRL for acting like such a retard.');
-       postedRecently = true;	   
-	   }
+		message.reply('HAHAHAHA fucking hilarious! Get it? It\'s the sex number, therefore it\'s funny! Except it\'s not fucking funny, how much of a braindead person do you have to be to find the number sixty-nine funny? How old are you people? Get a fucking grip on reality you mongoloid bastards. This is why i fucking despise meme culture sometimes, it promotes braindead humor for 3 year olds. "Nice nice nice" shut the fuck up you prick, repeating a word is not funny either. Holy fuck i\'d actually punch someone in the face IRL for acting like such a retard.'); 
+		     setTimeout(() => {
+        }, 43200000); //12hours  
+    }
     if ((message.content.toLowerCase() === ('oof') || message.content.toLowerCase() === ('oof 100'))) { // if that message matches ping
 
        message.reply('Shut the fuck up you absolute lobotomite. You think adding your poorly edited Skyrim makes this funny? What a joke. Get a grip on reality, stop saying "oof" during a serious conversation just because your two braincells cant ever send signals to eachother to form a response. Honestly pathetic. Oh guys look at me Im so funny OOF 100! Its so funny and quirky because its a roblox noise right guys? Hahah skyrim reference!! Shut the fuck up you absolute fucking dusty toilet. I would flush you down the drain in an instant.');	   
-       postedRecently = true;
+	     setTimeout(() => {
+        }, 43200000); //12hours
        }
     if (message.content.toLowerCase().includes('fortnite bad')) { // if that message matches ping
 
        message.reply('DUHHHHH FORTNITE BAD DUHHHH BRORTNITE BRAD!!1!1 IS THAT ALL YOU DEPRESSED FUCKS CAN SAY? FORTNITE FORTNITE EMOJI INSTA BAD EMOJI EMOJI BAD BAD!!1!1 I FEEL LIKE IM IN A FUCKING ASYLUM FULL OF DEMENTIA RIDDEN OLD PEOPLE WHO CAN DO NOTHING BUT REPEAT THE SAME FUCKING WORDS ON LOOP LIKE A BROKEN FUCKING RECORD');	   
-       postedRecently = true;
+	     setTimeout(() => {
+        }, 43200000); //12hours
        }
     if ((message.content.toLowerCase().includes('war') || message.content.toLowerCase().includes('violence'))) { // if that message matches ping
 
        message.reply('Did you know, the first ever Call of Duty game released in 2003, a whopping 4713 years after the first ever recorded war, "Sumer vs. Elam", which occurred in 2700 BC. This is strange, considering that violence didn\'t exist before Call of Duty. It is theorized that violent video games existed in that period, as it would be impossible for any type of violence to happen if they were not. In late 2012, a paleontologist who was digging into a random patch of land, found a strange rock case, when it was cracked open, a disc titled "Call of Duty: Before Christ" was inside, but the disc wasn\'t able to be read, despite being tested in various disc readers. We can however speculate, that this is what led to violence in our world, starting with the "Sumer vs. Elam" war, and continuing, with war looming over us to this day.');	   
-       postedRecently = true;
-       }
+    	     setTimeout(() => {
+        }, 43200000); //12hours   
+    }
    if (message.content.toLowerCase() === ('k')) { // if that message matches ping
 
        message.reply('K? Really you fucking idiot? I pour my heart and soul into a message and you reply with k? Do you think this is some kind of joke? Do you not realize how much this hurt me? This is like giving a man you risked his life in combat to save 100 other soldiers and giving him a bar of chocolate. God you are so inconsiderate. Come back when you learn to text like a normal person.');	   
-       postedRecently = true;
+       	     setTimeout(() => {
+        }, 43200000); //12hours
        }
    if ((message.content.toLowerCase().includes('torture') || message.content.toLowerCase().includes('cock and ball') || message.content.toLowerCase().includes('cbt'))) { // if that message matches ping
 
        message.reply('Why do people enjoy cock and ball torture? The act of intentionally or accidentally bringing pain upon the male genitals is typically a thought that people wince at with great force. However, there is a reason why this is an enjoyable experience. One rooted in the most important of sciences. Physics. Take, for example, a bottle of Tobasco Sauce. If one where to move the bottle up and down in a motion reminiscent to the one used during male self-stimulation it typically results in a moderate amount of sauce exiting the bottle. If struck hard on the bottom, however, a large amount will spurt out. Should this be done with the name genitals, turning them upside down and then striking the bottom of the scrotum with moderate to immense force, this will result in a large amount of seed to be ejected from the penis due to the energy being transferred from the palm of the hand to genitals. Therefore, because of physics and the transfer of energy from palm to the genitals, cock and ball torture is a scientifically pleasurable experience.');	   
-       postedRecently = true;
+       	     setTimeout(() => {
+        }, 43200000); //12hours
        }
     if (message.content.toLowerCase().includes('shuckle')) { // if that message matches ping
 
        message.reply('Shuckle is the worst pokemon.It\'s horrendously ugly and utterly useless. I hate it. What fucking niche does Shuckle have in BH? It just sits there and does nothing. I hate it. It provokes a deep rooted anger within me whenever I see it. I want to drive on over to the fucking pokemon headquarters and kill it. If this was the pokemon world, I\'d push it off a fucking cliff. Noobs just bring shuckle as if it\'s actually useful. It\'s not. Shuckle deserves to die. He deserves to have his smug little yellow face smashed in with a hammer. Oh wow, it\'s a fucking turtle with atrocious hp and horrendous typing, how fucking useful, I\'ll use it in every BH team I make. NO. STOP IT. It deserves to burn in hell. Why is it so goddamn smug. You\'re a fucking mold, you have no life goals, you will never accomplish anything in life apart from pissing me off. When you die noone will mourn. I hope you die');	   
-       postedRecently = true;
+       	     setTimeout(() => {
+        }, 43200000); //12hours
 	}
     if ((message.content.toLowerCase().includes('deod') || message.content.toLowerCase().includes('deoxys-defense') || message.content.toLowerCase().includes('deoxys defense'))) { // if that message matches ping
 
        message.reply('Okay so I was just peacefully walking down the road as I usually do, minding my own business. Then all of a sudden a little disgusting 8-year old disabled kid suddenly logged in to PS on his phone, and started using DeoD in BH! I was furious! But I calmed down and got a hold of myself. Even though using DeoD is a horrible crime, he was only a child and mentally ill at that. But then he found a replay of Simple Smash DeoD sweeping, and he showed his mum! My entire body was shaking!!! How dare he offend me like that and spread pure evil into the world? So I walked over to him, and punched him so hard he was doing the wormdance on the ground. And then I shouted "DEOD BAD CRESSELIA GOOD!!!!" and then I trampled him to death while shitting profusely all over his corpse. Now obviously I expected that everyone would applause me for hindering the spread of evil and for doing gods work, but for some weird reason everyone just started screaming, and after a couple of minutes the police arrived and they arrested me?!?! What is happening??? The trial will begin soon, so I just wanna know, Am I The Asshole?');	   
-       postedRecently = true;
+      	     setTimeout(() => {
+        }, 43200000); //12hours
        }
     if ((message.content.toLowerCase().includes('uturn') || message.content.toLowerCase().includes('u-turn'))) { // if that message matches ping
 
        message.reply('Day 1035 of Gen 7 BH. All of my teams lose to U-turn. Whenever I load up a team on the BH ladder, I\'m terrified that I\'ll run into a U-turn team. Fearing the worst, I have taken every possible countermeasure. My teams are now built exclusively with 4x bug resists. But still, U-turn is too powerful for me. As soon as I see the Registeel hit my Ho-Oh for 3%, I know the game is over right there and just click X before all my Pokemon inevitably die. Skylake is telling me to adapt but I don\'t know if I can anymore. I tried running Flame Body on my Togekiss but it\'s only a 30% chance that I\'m even able to punish the move. I urge every one of you to get this move out of the metagame immediately, because I don\'t think I can take it any more.');	   
-       postedRecently = true;
-	   }
- 
- setTimeout(() => {
-          postedRecently = false;
-        }, 600000); //10min
+		     setTimeout(() => {
+        }, 43200000); //12hours  
+    }
 	   
 	}
        });
