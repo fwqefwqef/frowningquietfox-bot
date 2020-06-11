2ade93953441e99da854bee07339e5706b4b7f95
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2020-06-11 15:32:18 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 client.on('ready', () => { // know if our bot is online
-    client.user.setActivity('Use !help', {type: "playing"}); 
+    client.user.setPresence({ activity: { name: '!help' }, status: 'online' });
 	//playing, streaming, listening, watching, custom_status
 
 });
