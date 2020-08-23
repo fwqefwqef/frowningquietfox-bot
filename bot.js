aa8ffabee469e3aa2a239b288432032e643dd265
fwqefwqef <46618311+fwqefwqef@users.noreply.github.com>
2020-08-23 19:10:14 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,4 +62,4 @@ client.on('message', message => { // When the message a message is executed
  
 // THIS  MUST  BE  THIS  WAY
 
-client.login("process.env.BOT_TOKEN");//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
